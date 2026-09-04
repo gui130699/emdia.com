5ea2733a501c911679dd691b3c3fd4eca8827a4a
gui130699 <guilhermeschmitt1306@gmail.com>
2026-09-03 21:42:44 -0300

    Updates

--- assets/startOfMonth-BUf9__u6.js
+++
@@ -1,1 +0,0 @@
-import{W as e}from"./index-tjhDpBHC.js";function t(t,n){let r=e(t,n?.in);return r.setDate(1),r.setHours(0,0,0,0),r}export{t};
---
+++ assets/startOfMonth-p4ESCfk1.js
@@ -0,0 +1,1 @@
+import{W as e}from"./index-BVQmw6j2.js";function t(t,n){let r=e(t,n?.in);return r.setDate(1),r.setHours(0,0,0,0),r}export{t};
