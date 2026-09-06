60482061f8d2489ff198c6d2b0741fff5d3efbba
gui130699 <guilhermeschmitt1306@gmail.com>
2026-09-06 18:12:13 -0300

    Updates

--- assets/ellipsis-vertical-DQxy-ZS8.js
+++
@@ -1,1 +0,0 @@
-import{yt as e}from"./index-DShV2h9B.js";var t=e(`ellipsis-vertical`,[[`circle`,{cx:`12`,cy:`12`,r:`1`,key:`41hilf`}],[`circle`,{cx:`12`,cy:`5`,r:`1`,key:`gxeob9`}],[`circle`,{cx:`12`,cy:`19`,r:`1`,key:`lyex9k`}]]);export{t};
