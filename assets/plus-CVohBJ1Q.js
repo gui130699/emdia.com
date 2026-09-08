c5a0429f1d0a47eb86e597faad96a91b2f95d2f4
gui130699 <guilhermeschmitt1306@gmail.com>
2026-09-08 10:51:02 -0300

    Updates

---
+++ assets/plus-CVohBJ1Q.js
@@ -0,0 +1,1 @@
+import{bt as e}from"./index-DA1lYNca.js";var t=e(`plus`,[[`path`,{d:`M5 12h14`,key:`1ays0h`}],[`path`,{d:`M12 5v14`,key:`s699le`}]]);export{t};
---
+++ assets/startOfMonth-DCldHieF.js
@@ -0,0 +1,1 @@
+import{q as e}from"./index-DA1lYNca.js";function t(t,n){let r=e(t,n?.in);return r.setDate(1),r.setHours(0,0,0,0),r}export{t};
